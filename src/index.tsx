d4cd27fed6c8d2dfb54c57e86b6014dbe6044f61
TheusH7X5 <matheush7x5@gmail.com>
2023-07-10 20:14:27 -0300

    ajuste

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import AppRouter from 'routes/routes';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <AppRouter />
